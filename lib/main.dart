3a051a1a3285e28f3ba12a0e9aac8743ccab4802
Essam Eldeen <3ssameldeen97@gmail.com>
2022-06-17 13:49:35 +0200

    start home screen

--- lib/main.dart
+++ lib/main.dart
@@ -61,8 +61,8 @@ class MyApp extends StatelessWidget {
         theme: lightTheme,
         darkTheme: darkTheme,
         themeMode: ThemeMode.light,
-        // home: const LoginScreen(),
-        home: startingPage,
+        home: const LoginScreen(),
+        // home: startingPage,
       ),
     );
   }
